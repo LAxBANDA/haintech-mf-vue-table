dec0436dd5ae35436e2efb3594cefda9d22a22c8
Alekos <alekos.georgudis@t4g.tech>
2023-03-08 11:09:53 -0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="container">
+    <Table></Table>
+  </div>
+</template>
+
+<script lang="ts">
+  import { defineComponent } from 'vue'
+import Table from './Table.vue';
+
+  export default defineComponent({ components: { Table } });
+</script>
